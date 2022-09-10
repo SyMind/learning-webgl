a1fe52850b875962d5aea35fc233cc13a0d72adb
SyMind <dacongsama@live.com>
2022-09-10 12:23:36 +0800

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
-  basePath: '/webgl-study-room',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/webgl-study-room' : ''
+  basePath: isProduction ? '/webgl-study-room' : '',
+  assetPrefix: isProduction ? '/webgl-study-room' : ''
 }
 
 module.exports = nextConfig
